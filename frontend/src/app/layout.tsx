0433a919b6a7df0a860c60d06a2e327a9b0baaeb
atjoao <130053141+atjoao@users.noreply.github.com>
2024-02-28 23:26:27 +0000

    adicionar um o

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Concord",
+    title: "Concordo",
     description: "@atjoao",
 };
 
