c77e6819085eb26d61c8312cccc9e23da5984985
atjoao <130053141+atjoao@users.noreply.github.com>
2024-03-03 09:04:02 +0000

    log ffmpeg on crashes to know what happened

--- backend/controllers/filesController.js
+++ backend/controllers/filesController.js
@@ -239,27 +239,36 @@ export const downloadFile = async (req, res) => {
                 }
 
                 if (ffmpegDetect) {
-                    ffmpeg(fileLocation)
-                        .screenshots({
-                            count: 1,
-                            folder: thumbDir,
-                            filename: `${checkForFile.fileName}.png`,
-                            size: "320x240",
-                        })
-                        .on("end", () => {
-                            try {
-                                data = readFileSync(thumbFullPath);
-                                res.setHeader("Content-Length", data.length);
-                                res.setHeader(
-                                    "Content-Disposition",
-                                    `filename="${checkForFile.fileName}.png"`
-                                );
+                    try {
+                        ffmpeg(fileLocation)
+                            .screenshots({
+                                count: 1,
+                                folder: thumbDir,
+                                filename: `${checkForFile.fileName}.png`,
+                                size: "320x240",
+                            })
+                            .on("end", () => {
+                                try {
+                                    data = readFileSync(thumbFullPath);
+                                    res.setHeader(
+                                        "Content-Length",
+                                        data.length
+                                    );
+                                    res.setHeader(
+                                        "Content-Disposition",
+                                        `filename="${checkForFile.fileName}.png"`
+                                    );
 
-                                return res.send(data);
-                            } catch (error) {
-                                return res.status(200);
-                            }
-                        });
+                                    return res.send(data);
+                                } catch (error) {
+                                    return res.status(200);
+                                }
+                            });
+                    } catch (error) {
+                        console.log(
+                            "FFMPEG happened to crash while trying to take a screenshot"
+                        );
+                    }
                 }
             }
         } else {
@@ -307,29 +316,37 @@ export const downloadFile = async (req, res) => {
                     );
 
                     if (!streaming) {
-                        ffmpeg(fileLocation)
-                            .toFormat("mp4")
-                            .videoCodec("copy")
-                            .audioCodec("aac")
-                            .save(convertedFilePath)
-                            .on("end", () => {
-                                convertedFiles[fileLocation] =
-                                    convertedFilePath;
-                                const fileStream = createReadStream(
-                                    convertedFilePath,
-                                    { emitClose: true }
-                                );
+                        try {
+                            ffmpeg(fileLocation)
+                                .toFormat("mp4")
+                                .videoCodec("copy")
+                                .audioCodec("aac")
+                                .save(convertedFilePath)
+                                .on("end", () => {
+                                    convertedFiles[fileLocation] =
+                                        convertedFilePath;
+                                    const fileStream = createReadStream(
+                                        convertedFilePath,
+                                        {
+                                            emitClose: true,
+                                        }
+                                    );
 
-                                fileStream.pipe(stream);
+                                    fileStream.pipe(stream);
 
-                                fileStream.on("close", function () {
-                                    if (!fileStream.destroyed)
-                                        fileStream.destroy();
+                                    fileStream.on("close", function () {
+                                        if (!fileStream.destroyed)
+                                            fileStream.destroy();
+                                    });
+                                })
+                                .on("error", (error) => {
+                                    console.log(error);
                                 });
-                            })
-                            .on("error", (error) => {
-                                console.log(error);
-                            });
+                        } catch (error) {
+                            console.log(
+                                "FFMPEG happened to crash while trying to stream "
+                            );
+                        }
                     }
 
                     stream.pipe(res);
