ffc6a27881692bcd8a9c768eeb95b1cb39162cc1
atjoao <130053141+atjoao@users.noreply.github.com>
2024-05-06 01:50:42 +0100

    aa

--- server/functions/events/emitVerificationCompleted.js
+++ server/functions/events/emitVerificationCompleted.js
@@ -2,10 +2,9 @@ import cache from "../../cache/index.js";
 
 export default async function (io, data) {
     const sockets = await cache.getSocketsByUserId(data.userId);
-    console.log(sockets);
+
     if (Array.isArray(sockets)) {
         sockets.forEach((sid) => {
-            console.log(sid);
             io.to(sid).emit("verificationCompleted", true);
         });
     }
